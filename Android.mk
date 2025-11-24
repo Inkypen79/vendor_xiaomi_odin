70e905adfe1afb4d6851b2d132e5bc239bf13af4
Nicholas Magill <nmagill1979@gmail.com>
2025-11-24 23:57:58 +0000

    odin: Import device firmware
    * from OS1.0.4.0.UKMCNXM

--- Android.mk
+++ Android.mk
@@ -3,3 +3,25 @@
 #
 
 LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),odin)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,cbb60492e5e70a776be1524229a0c8128b19ce62)
+$(call add-radio-file-sha1-checked,radio/aop.img,b57ff05d92945eebd4cd186d1042021d5a44e4c7)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,f5db0e73e600897858af1d4a4672ccc640708388)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,d36c27132e0df35eb52fb4c13f59ecf16491e36b)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,150669396d68349b350836c9efe3660a2269445c)
+$(call add-radio-file-sha1-checked,radio/dsp.img,2591f329ad47cd8a086c8ff0fd55c9ceb04554a7)
+$(call add-radio-file-sha1-checked,radio/featenabler.img,7459718f7affc26408d567d3d9d87557ff792853)
+$(call add-radio-file-sha1-checked,radio/hyp.img,22cee4549e32857ba957ff8fd29fd87acc105487)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,ac4c5e0788d11f5775a269627d16092b146fbba9)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,cc942ccadaed4935b6b38f3f7d82e31e49692ba4)
+$(call add-radio-file-sha1-checked,radio/modem.img,f47e99d471a4c16f6d34d47ad86b8ec539bc77ea)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,13996517158fec0db0d2f8bbb94b18a8ae778a84)
+$(call add-radio-file-sha1-checked,radio/shrm.img,98a0fcf0f313f445fca98ab519c9a7b1a0b23107)
+$(call add-radio-file-sha1-checked,radio/tz.img,7ab940716045e6a482dd3b75089c4be00bb4fb2e)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,26ab749028b94c0dbbbae7be3c881baf47f78354)
+$(call add-radio-file-sha1-checked,radio/xbl.img,b0bf0d4246679c190f6b6636e5668e8c6606e3c3)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,a39dcbffa24486caec8334e4dbfa65d1c699af6f)
+
+endif
